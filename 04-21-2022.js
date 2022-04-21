509e1476c12c6bd2fe0cc6e186c8cd76dede1c4b
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-04-21 17:05:44 -0400

    added anothercodewars: given an array of numbers, return the inverse of each

--- 04-21-2022.js
+++ 04-21-2022.js
@@ -10,3 +10,9 @@ function findNeedle(haystack) {
    let found = haystack.indexOf('needle')
    return `found the needle at position ${found}`
 }
+
+/* Given a set of numbers, return the additive inverse of each.Each positive becomes negatives, and the negatives become positives. */
+
+function invert(array) {
+   return array.map(x => -x)
+}
