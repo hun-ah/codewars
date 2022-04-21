983d92a890c9141a87e533e4ecb8fce8a6fa28d5
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-04-21 19:15:03 -0400

    new codewars: find the mean of an array

--- 04-21-2022.js
+++ 04-21-2022.js
@@ -28,3 +28,14 @@ function past(h, m, s) {
    let timeInMilliseconds = timeInSeconds * 1000
    return timeInMilliseconds
 }
+
+/* It's the academic year's end, fateful moment of your school report.The averages must be calculated.All the students come to you and entreat you to calculate their average for them.Easy! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty. */
+
+function getAverage(marks) {
+   let mean = marks.reduce((a, c) => a + c / marks.length, 0)
+   return Math.floor(mean)
+}
