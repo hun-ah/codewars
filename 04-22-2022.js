558cccb4aeab79ab486eea54a68d1492018c9137
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-04-22 16:40:14 -0400

    added codewars: turn string into binary

--- 04-22-2022.js
+++ 04-22-2022.js
@@ -1,3 +1,10 @@
 /* Complete the solution so that it reverses all of the words within the string passed in. */
 
 const reverseWords = str => str.split(' ').reverse().join(' ')
+
+/* Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'.Return the resulting string. */
+
+function fakeBin(x) {
+   let arr = x.split('')
+   return arr.map(i => (i < 5) ? '0' : '1').join('')
+}
