6cbc44cd53b5a929f2665de3169dd43c16f629b5
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-05-08 21:47:33 -0500

    create function that returns an array n multiples of x

--- 05-08-2022.js
+++ 05-08-2022.js
@@ -10,3 +10,17 @@ const areaOrPerimeter = function (l, w) {
       return perimeter
    }
 }
+
+/* Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array (or list in Python, Haskell or Elixir). */
+
+function countBy(x, n) {
+   let arr = []
+   for (let i = 1; i <= n; i++) {
+      arr.push(i)
+   }
+   return arr.map(e => e * x)
+}
