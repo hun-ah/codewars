bcae346cde50f05a42dde483235234896ec690b6
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-05-13 20:14:13 -0400

    create function that counts points and returns total

---
+++ 05-13-2022.js
@@ -0,0 +1,23 @@
+/* Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point */
+
+function points(games) {
+   let total = 0
+   games.forEach(game => {
+      if (game[0] > game[2]) {
+         total += 3
+      } else if (game[0] === game[2]) {
+         total += 1
+      } else {
+         total += 0
+      }
+   })
+   console.log(total)
+}
