d640c674a1ab71e0dd744669426b68ec2c9a0b70
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-05-13 20:19:29 -0400

    create function that returns third angle of triangle

--- 05-13-2022.js
+++ 05-13-2022.js
@@ -21,3 +21,11 @@ function points(games) {
    })
    console.log(total)
 }
+
+/* You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd. */
+
+function otherAngle(a, b) {
+   return 180 - (a + b)
+}
