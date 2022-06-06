0b7cd67f41ea8668974273397e3d67daa4b4d60e
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-06-06 15:09:53 -0400

    convert human years to cat and dog years

--- 06-06-2022.js
+++ 06-06-2022.js
@@ -17,3 +17,51 @@ Create a function for the terminal game that takes the current position of the h
 function move(position, roll) {
    return position + roll * 2
 }
+
+/* I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that */
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+   let ages = []
+   let catYears = 0
+   let dogYears = 0
+
+   ages.push(humanYears)
+
+   if (humanYears === 1) {
+      catYears += 15
+      dogYears += 15
+      ages.push(catYears, dogYears)
+   }
+
+   if (humanYears === 2) {
+      catYears += 24
+      dogYears += 24
+      ages.push(catYears, dogYears)
+
+   }
+
+   if (humanYears > 2) {
+      catYears += (humanYears - 2) * 4 + 24
+      dogYears += (humanYears - 2) * 5 + 24
+      ages.push(catYears, dogYears)
+
+   }
+   return ages
+}
