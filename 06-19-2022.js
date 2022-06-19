0123f3521c0116483e86d6ef699330447b934590
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-06-19 09:31:57 -0400

    return new array with either integer square root of element or sqaure the number

--- 06-19-2022.js
+++ 06-19-2022.js
@@ -3,3 +3,15 @@
 function nthEven(n) {
    return (n - 1) * 2
 }
+
+/* Write a method, that will get an integer array as parameter and will process every number from this array.
+
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+ */
+
+const squareOrSquareRoot = a => {
+   return a.map(e => Math.sqrt(e) % 1 === 0 ? Math.sqrt(e) : e * e)
+}
