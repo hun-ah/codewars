b6e0d372d61576900a1cb4bb3cf3a7e4b77ae292
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-06-21 21:01:28 -0400

    create method that turns uppercase letters into lowercase and vice versa

--- 06-21-2022.js
+++ 06-21-2022.js
@@ -5,3 +5,15 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 function squareDigits(num) {
    return Number(num.toString().split('').map(e => e * e).join(''))
 }
+
+/* Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. */
+
+String.prototype.toAlternatingCase = function () {
+   return this.split('').map((e) => {
+      if (e === e.toLowerCase()) {
+         return e.toUpperCase()
+      } else if (e === e.toUpperCase()) {
+         return e.toLowerCase()
+      }
+   }).join('')
+}
