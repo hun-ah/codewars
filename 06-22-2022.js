a22ec5b71aeeab2ca42702a7509aceb64bb1e458
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-06-22 15:50:31 -0400

    check to see if string is all caps

--- 06-22-2022.js
+++ 06-22-2022.js
@@ -17,3 +17,20 @@ function expressionMatter(a, b, c) {
 
    return highest
 }
+
+/* Create a method to see whether the string is ALL CAPS.
+
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+*/
+
+String.prototype.isUpperCase = function () {
+   let answer = true
+   this.split('').forEach(e => {
+      if (e === e.toUpperCase() || e === '') {
+         answer
+      } else {
+         answer = false
+      }
+   })
+   return answer
+}
