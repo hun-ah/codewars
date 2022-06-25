af52f3d6cdf6733ae80bad167344ab580477c67f
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-06-25 12:06:14 -0400

    return ASCII character from number value

--- 06-25-2022.js
+++ 06-25-2022.js
@@ -24,3 +24,9 @@ function xor(a, b) {
       return true
    }
 }
+
+/* Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value. */
+
+function getChar(c) {
+   return String.fromCharCode(c)
+}
