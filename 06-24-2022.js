c5b39e0b42e21bb839a8df3734026b19daa3bede
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-06-24 22:15:11 -0400

    return the nth-power of element in array with the index n

--- 06-24-2022.js
+++ 06-24-2022.js
@@ -9,3 +9,17 @@ function powersOfTwo(n) {
    }
    return arr
 }
+
+/* You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0. */
+
+function index(array, n) {
+   let result = -1
+   for (let i = 0; i < array.length; i++) {
+      if (!array[n]) {
+         result
+      } else {
+         result = Math.pow(array[n], n)
+      }
+   }
+   return result
+}
