1eb1ba767694a50543668e2950602184f1350334
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-06-26 11:51:13 -0400

    return array of numbers from number at first index to number at last index, incremented by 1

--- 06-26-2022.js
+++ 06-26-2022.js
@@ -9,3 +9,13 @@ All inputs will be integers. Please return an integer. Round down. */
 function dutyFree(normPrice, discount, hol) {
    return Math.floor(hol / (normPrice * discount / 100))
 }
+
+/* Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included). */
+
+function pipeFix(numbers) {
+   let arr = []
+   for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+      arr.push(i)
+   }
+   return arr
+}
