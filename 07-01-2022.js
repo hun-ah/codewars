0eddef5a042dbef8996a7287b8abba26ffc94577
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-07-01 09:46:18 -0400

    debug code

--- 07-01-2022.js
+++ 07-01-2022.js
@@ -5,3 +5,21 @@ function formatMoney(amount) {
    return `$${input.toFixed(2)}`
 }
 
+/* Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius. Unfortunately his code has some bugs.
+
+Find the errors in the code to get the celsius converter working properly.
+
+ */
+
+function weatherInfo(temp) {
+   let c = convertToCelsius(temp)
+   if (c < 0)
+      return (c + " is freezing temperature")
+   else
+      return (c + " is above freezing temperature")
+}
+
+function convertToCelsius(temperature) {
+   var celsius = (temperature - 32) * 5 / 9
+   return celsius
+}
