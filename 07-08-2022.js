5c0dcf7058dac1f63f0023feb987b39c308da3a1
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-07-08 17:44:11 -0400

    reverse elements in array

---
+++ 07-08-2022.js
@@ -0,0 +1,11 @@
+/* I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing. */
+
+// PREP
+// P: array with 2 elements
+// R: array with values swapped
+// E: [1,2] returns [2,1] || [6,9] returns [9,6]
+// P: // reverse elements in array
+
+function swapValues(arr) {
+   return arr.reverse()
+}
