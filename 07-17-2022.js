d5091e08599121e07e52a702866980c303e88a24
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-07-17 20:40:41 -0400

    reverse words in string

---
+++ 07-17-2022.js
@@ -0,0 +1,13 @@
+// PREP
+// P: string
+// R: string reversed
+// E: 'Hello World' returns 'World Hello'
+// P:
+
+function reverse(string) {
+   // take in a string
+   // separate words in string
+   // reverse words
+   // join them back together
+   return string.split(' ').reverse().join(' ')
+}
