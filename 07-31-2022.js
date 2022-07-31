43e60c4fd52c762e7d195cbd2bfe4539ccf9b679
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-07-31 17:50:08 -0400

    concatenate first and last name

---
+++ 07-31-2022.js
@@ -0,0 +1,3 @@
+/* Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name. */
+
+const combineNames = (fn, ln) => fn + ' ' + ln
