7cfa426e5433b538bb13ff7718cd56c9ccd4c71c
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-08-13 19:24:00 -0400

    check if element in array is even or odd and push to new array

---
+++ 08-13-2022.js
@@ -0,0 +1,15 @@
+/* Coding in function pickIt, function accept 1 parameter:arr, it's a number array, we need traverse arr by using for loop, if element is odd number, push it to array odd, if it's a even number, push it to array even. */
+
+function pickIt(arr) {
+   var odd = [], even = [];
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+         even.push(arr[i])
+      } else {
+         odd.push(arr[i])
+      }
+   }
+
+
+   return [odd, even];
+}
