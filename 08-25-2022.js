64b4d6b6b787dac5dbb245746d0a65d9f00471d2
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-08-25 20:33:18 -0400

    template string practice

---
+++ 08-25-2022.js
@@ -0,0 +1,6 @@
+/* Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+ */
+
+var TempleStrings = function (obj, feature) {
+   return `${obj} are ${feature}`
+}
