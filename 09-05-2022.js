f939981f3cda20b3a86801964de46235ae7b73d4
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2022-09-05 17:33:28 -0400

    return a string with every word in the parameter given with another word

---
+++ 09-05-2022.js
@@ -0,0 +1,9 @@
+/* In this kata, the function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should return an empty string. */
+
+function toFreud(string) {
+   if (string === '') {
+      return ''
+   } else if (string.length > 0) {
+      return string.split(' ').map(word => 'sex').join(' ')
+   }
+}
