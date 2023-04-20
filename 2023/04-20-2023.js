e5a62ffc61c7bc435b0a3260926ae7c2b7ac62f4
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-04-20 10:50:18 -0400

    determine number of cols/rows behind person

---
+++ 2023/04-20-2023.js
@@ -0,0 +1,19 @@
+// given total # of rows/columns in theatre and accounting for the the row/column I am in...
+//  => # of ppl sitting directly behind me & to the left
+
+// inputs: total # of rows & columns in the theatre
+// inputs: row and column I am in
+
+function seatsInTheater(nCols, nRows, col, row) {
+   // total = 176 seats
+   // people to my right: nCols - (col - 1) = 12 seats
+   // people behind me: nRows - row = 8 sets
+   // return 8 * 12
+   return (nCols - (col - 1)) * (nRows - row)
+}
+
+console.log(seatsInTheater(16, 11, 5, 3), 96)
+console.log(seatsInTheater(1, 1, 1, 1), 0)
+console.log(seatsInTheater(13, 6, 8, 3), 18)
+console.log(seatsInTheater(60, 100, 60, 1), 99)
+console.log(seatsInTheater(1000, 1000, 1000, 1000), 0)
