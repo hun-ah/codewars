eeab3e5761be849b930beede2161731ad6352b2c
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-04-21 20:53:47 -0400

    check for letter and casing

--- 2023/04-21-2023.js
+++ 2023/04-21-2023.js
@@ -13,3 +13,35 @@ function squareArea(A) {
 console.log(squareArea(2), 1.62)
 console.log(squareArea(0), 0)
 console.log(squareArea(14.05), 80)
+
+// check if not a letter => -1
+// check for same case => 1
+// check if letter => 0
+// A-Z = 65-90
+// a-z = 97-122
+function sameCase(a, b) {
+   let nums = []
+   for (let i = 65; i <= 122; i++) {
+      nums.push(i)
+   }
+   nums.splice(nums.indexOf(91), 6)
+
+   let result
+
+   if (nums.includes(a.charCodeAt(0)) && nums.includes(b.charCodeAt(0))) {
+      result = 0
+   } else {
+      result = -1
+   }
+
+   if ((a.charCodeAt(0) >= 65 && a.charCodeAt(0) <= 90) && (b.charCodeAt(0) >= 65 && b.charCodeAt(0) <= 90) || (a.charCodeAt(0) >= 97 && a.charCodeAt(0) <= 122) && (b.charCodeAt(0) >= 97 && b.charCodeAt(0) <= 122)) {
+      result += 1
+   }
+
+   return result
+}
+
+console.log(sameCase('C', 'B'), 1)
+console.log(sameCase('b', 'a'), 1)
+console.log(sameCase('c', 'B'), 0)
+console.log(sameCase('H', ':'), -1)
