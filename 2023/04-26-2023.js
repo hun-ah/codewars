07e2daa0e29669c321c0e8d5ccf3a4dcfd854849
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-04-26 09:13:37 -0400

    return color from Ghost class

--- 2023/04-26-2023.js
+++ 2023/04-26-2023.js
@@ -1,22 +1,32 @@
-function binToDec(bin) {
-   return bin.split('').reverse().map((e, i) => e * Math.pow(2, i)).reduce((acc, c) => acc + c)
-}
+// function binToDec(bin) {
+//    return bin.split('').reverse().map((e, i) => e * Math.pow(2, i)).reduce((acc, c) => acc + c)
+// }
 
-console.log(binToDec('1001001'), 73)
-console.log(binToDec('10110'), 22)
-console.log(binToDec('0'), 0)
-console.log(binToDec('1'), 1)
+// console.log(binToDec('1001001'), 73)
+// console.log(binToDec('10110'), 22)
+// console.log(binToDec('0'), 0)
+// console.log(binToDec('1'), 1)
 
-function pillars(numPillars, pillarDistance, pillarWidth) {
-   // find width between first and last pillars in cm (not incl f/l)
-   const pilDist = (numPillars - 1) * pillarDistance * 100
-   // find width of remaining pillars
-   const otherPillWidth = numPillars * pillarWidth
-   // width of f/l pillars
-   const firstLast = pillarWidth * 2
-   return pilDist + otherPillWidth - firstLast
-}
+// function pillars(numPillars, pillarDistance, pillarWidth) {
+//    // find width between first and last pillars in cm (not incl f/l)
+//    const pilDist = (numPillars - 1) * pillarDistance * 100
+//    // find width of remaining pillars
+//    const otherPillWidth = numPillars * pillarWidth
+//    // width of f/l pillars
+//    const firstLast = pillarWidth * 2
+//    return pilDist + otherPillWidth - firstLast
+// }
 
-console.log(pillars(1, 10, 10), 0)
-console.log(pillars(2, 20, 25), 2000)
-console.log(pillars(11, 15, 30), 15270)
+// console.log(pillars(1, 10, 10), 0)
+// console.log(pillars(2, 20, 25), 2000)
+// console.log(pillars(11, 15, 30), 15270)
+
+
+var Ghost = function () {
+   // based on the corresponding index, return that string (choose randomly)
+   const colors = ['white', 'yellow', 'purple', 'red']
+   this.color = colors[Math.floor(Math.random() * colors.length)]
+};
+
+const ghost = new Ghost()
+console.log(ghost.color)
